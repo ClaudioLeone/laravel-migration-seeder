df7280d67590e8ea8f5a458921de67740da88e71
ClaudioLeone <claleone92@gmail.com>
2023-05-28 20:59:56 +0200

    Adjusted 'home' routing syntax in navbar hrefs, created table and executed migration

---
+++ database/migrations/2023_05_28_183102_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->integer('train_code')->unsigned();
+            $table->tinyInteger('num_of_wagons')->unsigned();
+            $table->boolean('is_in_time')->default(true);
+            $table->boolean('is_cancelled')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,9 +2,9 @@
     <h1 class="text-center my-3"><em>Sito dei Treni</em></h1>
 
     <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid justify-content-start">
-          <a class="navbar-brand navbar-icon" href="/">🏡 Home</a>
-          <a class="navbar-brand navbar-icon" href="/">🚅 Treni</a>
+        <div class="container-fluid justify-content-center py-3 ms-navbar-bg">
+          <a class="navbar-brand navbar-icon" href="{{ route('home') }}">🏡 Home</a>
+          <a class="navbar-brand navbar-icon" href="{{ route('home') }}">🚅 Treni</a>
         </div>
     </nav>
 </header>
